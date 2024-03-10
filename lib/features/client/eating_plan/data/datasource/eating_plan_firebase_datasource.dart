757161e5f58a698c6f432064fac09bf83d08d94c
Fernando Luis <fernando.martinez@soytul.com>
2024-03-09 22:16:00 -0600

    Add crashlytics

--- lib/features/client/eating_plan/data/datasource/eating_plan_firebase_datasource.dart
+++ lib/features/client/eating_plan/data/datasource/eating_plan_firebase_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:healthy_app/core/extensions/datetime.dart';
 import 'package:healthy_app/features/client/eating_plan/data/datasource/eating_plan_datasource.dart';
 import 'package:healthy_app/features/client/eating_plan/domain/entities/eating_plan_entity.dart';
@@ -8,39 +9,45 @@ import 'package:healthy_app/features/client/eating_plan/domain/entities/food_blo
 class EatingPlanFirebaseDatasource implements EatingPlanDatasource {
   final FirebaseAuth _auth;
   final FirebaseFirestore _firestore;
+  final FirebaseCrashlytics _crashlytics;
 
-  EatingPlanFirebaseDatasource(this._auth, this._firestore);
+  EatingPlanFirebaseDatasource(this._auth, this._firestore, this._crashlytics);
 
   @override
   Future<EatingPlanEntity> getEatingPlan(DateTime date) async {
-    // Get checked food
-    final uid = _auth.currentUser!.uid;
-    final snapshotChecked = await _firestore
-        .collection('eating_plan_checked')
-        .where('userId', isEqualTo: uid)
-        .where('date', isEqualTo: date.removeTime())
-        .orderBy('createdAt', descending: true)
-        .limit(1)
-        .get();
+    try {
+      // Get checked food
+      final uid = _auth.currentUser!.uid;
+      final snapshotChecked = await _firestore
+          .collection('eating_plan_checked')
+          .where('userId', isEqualTo: uid)
+          .where('date', isEqualTo: date.removeTime())
+          .orderBy('createdAt', descending: true)
+          .limit(1)
+          .get();
 
-    if (snapshotChecked.docs.isEmpty) return EatingPlanEntity.initial();
+      if (snapshotChecked.docs.isEmpty) return EatingPlanEntity.initial();
 
-    final foodChecked = snapshotChecked.docs.first.data();
-    foodChecked['id'] = snapshotChecked.docs.first.id;
-    final eatingPlanId = foodChecked['eatingPlanId'];
+      final foodChecked = snapshotChecked.docs.first.data();
+      foodChecked['id'] = snapshotChecked.docs.first.id;
+      final eatingPlanId = foodChecked['eatingPlanId'];
 
-    // Get eating plan
-    final snapshot = await _firestore //
-        .collection('eating_plan')
-        .doc(eatingPlanId)
-        .get();
+      // Get eating plan
+      final snapshot = await _firestore //
+          .collection('eating_plan')
+          .doc(eatingPlanId)
+          .get();
 
-    final eatingPlanData = snapshot.data()!;
-    eatingPlanData['id'] = snapshot.id;
-    eatingPlanData['foodChecked'] = foodChecked;
+      final eatingPlanData = snapshot.data()!;
+      eatingPlanData['id'] = snapshot.id;
+      eatingPlanData['foodChecked'] = foodChecked;
 
-    final eatingPlan = EatingPlanEntity.fromMap(eatingPlanData);
-    return eatingPlan;
+      final eatingPlan = EatingPlanEntity.fromMap(eatingPlanData);
+      return eatingPlan;
+    } catch (e, s) {
+      await _crashlytics.recordError(e, s);
+      throw e;
+    }
   }
 
   @override
--- lib/features/client/initial_config/data/datasource/initial_config_firebase_datasource.dart
+++ lib/features/client/initial_config/data/datasource/initial_config_firebase_datasource.dart
@@ -1,28 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:healthy_app/features/client/initial_config/data/datasource/initial_config_datasource.dart';
 import 'package:healthy_app/features/client/initial_config/domain/entities/initial_config_entity.dart';
 
 class InitialConfigFirebaseDatasource implements InitialConfigDatasource {
   final FirebaseAuth _auth;
   final FirebaseFirestore _firestore;
+  final FirebaseCrashlytics _crashlytics;
 
-  InitialConfigFirebaseDatasource(this._auth, this._firestore);
+  InitialConfigFirebaseDatasource(
+    this._auth,
+    this._firestore,
+    this._crashlytics,
+  );
 
   @override
   Future<InitialConfigEntity?> getInitialConfig() async {
-    final uid = _auth.currentUser!.uid;
-    final snapshot = await _firestore //
-        .collection('client_config')
-        .doc(uid)
-        .get();
+    try {
+      final uid = _auth.currentUser!.uid;
+      final snapshot = await _firestore //
+          .collection('client_config')
+          .doc(uid)
+          .get();
 
-    if (!snapshot.exists) return null;
+      if (!snapshot.exists) return null;
 
-    final data = snapshot.data()!;
-    final initialConfig = InitialConfigEntity.fromMap(data);
+      final data = snapshot.data()!;
+      final initialConfig = InitialConfigEntity.fromMap(data);
 
-    return initialConfig;
+      return initialConfig;
+    } catch (e, s) {
+      await _crashlytics.recordError(e, s);
+      throw e;
+    }
   }
 
   @override
